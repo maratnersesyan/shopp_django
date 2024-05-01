fa5ac1883d2238527390eba206bee7cefe469ce3
maratnersesyan <49437619+maratnersesyan@users.noreply.github.com>
2024-05-01 18:50:23 +0300

    The main appis ready pages index, about, contact and delivery

--- main/views.py
+++ main/views.py
@@ -3,13 +3,35 @@ from django.shortcuts import render
 
 def index(request) -> HttpResponse:
     context = {
-        'title': 'home',
-        'content': 'content',
-        'list': ['first', 'second'],
-        'dist': {'first': 1, 'second': 'second'},
-        'bool': True
+        'title': 'Home - Главная',
+        'content': 'Магазин мебели HOME',
+
     }
     return render(request, 'main/index.html', context)
 
 def about(request) -> HttpResponse:
-    return HttpResponse('О Нас')
+    context = {
+        'title': 'Home - О Нас',
+        'content': 'Наши контакты',
+        'text_on_page': 'Текст о том, почему магазин такой классный'
+    }
+    return render(request, 'main/about.html', context)
+
+def delivery(request) -> HttpResponse:
+    context = {
+        'title': 'Способы доставки',
+        'content': 'Интернет-магазин выполняет доставку любого товара своей собственной Службой доставки.',
+    }
+    return render(request, 'main/delivery.html', context)
+
+def contact(request) -> HttpResponse:
+    context = {
+        'title': 'Контакты',
+        'tel': '8 (800) 234-50-60',
+        'time': 'Время работы Call-центра:',
+        'times': 'ежедневно с 10:00 до 20:00',
+        'content': 'В нашем магазине всегда только самые лучшие товары по низким ценам. Каталог продаваемых товаров постоянно пополняется и обновляется.',
+    }
+    return render(request, 'main/contact.html', context)
+
+
--- shop_django/settings.py
+++ shop_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-od(*(+7ry$k^f%fd7n6*-+y3ivt^t*kzs^gj1@$2fip6wlrpk('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -119,6 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / 'static'
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
